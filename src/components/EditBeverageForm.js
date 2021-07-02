88a2e5aad8cb72aa9ec1d8fd91192dc0ac0d3cf1
Thomas Russell <t.p.russell19@gmail.com>
2021-07-02 14:59:34 -0700

    :hammer: add code to editbeverageform

---
+++ src/components/EditBeverageForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
+
+function EditBeverageForm(props) {
+  const { beverage } = props;
+  function handleEditBeverageFormSubmission(event) {
+    event.preventDefault();
+    props.onEditBeverage({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      style: event.target.style.value,
+      abv: event.target.abv.value,
+      price: event.target.price.value,
+      quantity: event.target.quantity.value,
+      id: beverage.id
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditBeverageFormSubmission}
+        buttonText="Update Beverage"
+        />
+    </React.Fragment>
+  );
+}
+
+EditBeverageForm.propTypes = {
+  beverage: PropTypes.object,
+  onEditBeverage: PropTypes.func
+};
+
+export default EditBeverageForm;
