9c8988b9b12e295ca6e0af1d06511d381599ab30
Thomas Russell <t.p.russell19@gmail.com>
2021-07-02 12:09:14 -0700

    :hammer: add code to beverage

---
+++ src/components/Beverage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Beverage(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenBeverageClicked(props.id)}>
+        <h3>{props.name} --- ${props.price}</h3>
+        <p>{props.brand}</p>
+        <p>{props.style} || ABV: {props.abv}%</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Beverage.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  style: PropTypes.string.isRequired,
+  abv: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenBeverageClicked: PropTypes.func
+};
+
+export default Beverage;
