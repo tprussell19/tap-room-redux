a526f0fbbd64366015e4f9b9909f74626de822ce
Thomas Russell <t.p.russell19@gmail.com>
2021-07-09 10:56:19 -0700

    :hammer: add store provider at app level

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux';
+import reducer from './reducers/keg-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
