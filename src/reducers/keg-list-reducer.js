0a9d23cbd15a77ff3f722c82fff1fe8a0a1d82f1
Thomas Russell <t.p.russell19@gmail.com>
2021-07-09 09:53:24 -0700

    :hammer: add code to pass second test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, style, abv, price, pints, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          style: style,
+          abv: abv,
+          price: price,
+          pints: pints,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
