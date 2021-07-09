49ad79379250e43d7c6dfbf140268b3140f1df15
Thomas Russell <t.p.russell19@gmail.com>
2021-07-09 14:30:46 -0700

    :hammer: add addKeg action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,17 @@ export const deleteKeg = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addKeg = (keg) => {
+  const { name, brand, style, abv, price, pints, id } = keg;
+  return {
+    type: "ADD_KEG",
+    name: name,
+    brand: brand,
+    style: style,
+    abv: abv,
+    price: price,
+    pints: pints,
+    id: id
+  }
+}
