5ca2d69ebca16b586c88be2cc7922cddf6b5e4b5
Thomas Russell <t.p.russell19@gmail.com>
2021-07-09 12:00:40 -0700

    :test_tube: add failing test for form toggle

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -4,6 +4,10 @@ describe("formVisibleReducer", () => {
 
   test('Should return default state if no action is recognized', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
-  })
+  });
+
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+  });
 
 });
