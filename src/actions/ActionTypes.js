847c78136a163f251f08c40dd94b40a348454675
Thomas Russell <t.p.russell19@gmail.com>
2021-07-10 10:58:19 -0700

    :hammer: add action consts for selectedkegreducer

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,3 +1,5 @@
 export const ADD_KEG = 'ADD_KEG';
 export const DELETE_KEG = 'DELETE_KEG';
 export const TOGGLE_FORM = 'TOGGLE_FORM';
+export const SELECT_KEG = 'SELECT_KEG';
+export const CLEAR_SELECTED_KEG = 'CLEAR_SELECTED_KEG';
