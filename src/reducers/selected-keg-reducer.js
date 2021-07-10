4624190371fb8a67064f98b012b43ea7ebefe3d4
Thomas Russell <t.p.russell19@gmail.com>
2021-07-10 11:29:03 -0700

    :hammer: add code to successfully select a keg

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,7 +1,21 @@
 import * as c from './../actions/ActionTypes';
 
 const selectedKegReducer = (state = {}, action) => {
-  return state;
+  const { name, brand, style, abv, price, pints, id } = action;
+  switch (action.type) {
+    case c.SELECT_KEG:
+      return {
+        name: name,
+        brand: brand,
+        style: style,
+        abv: abv,
+        price: price,
+        pints: pints,
+        id: id
+      }
+    default:
+      return state;
+  }
 }
 
 export default selectedKegReducer;
