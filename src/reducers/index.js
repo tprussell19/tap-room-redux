f0f7c92c60f97eb08552903aa59cf84fe8059d13
Thomas Russell <t.p.russell19@gmail.com>
2021-07-10 12:48:16 -0700

    :hammer: add selectedKeg prop to rootReducer and refactor test for default state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from "./form-visible-reducer";
 import kegListReducer from "./keg-list-reducer";
+import selectedKegReducer from "./selected-keg-reducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterKegList: kegListReducer
+  masterKegList: kegListReducer,
+  selectedKeg: selectedKegReducer
 });
 
 export default rootReducer;
